dcccb81881db99267fe262047c2c95d357661898
William Li <wzl7zb@virginia.edu>
2019-03-28 17:40:35 -0400

    filter by month

--- dindin/components/CalendarList.js
+++ dindin/components/CalendarList.js
@@ -42,19 +42,31 @@ const DATA = [
         time: "12:00 PM",
         imgurl: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/46412675_2508496152554310_2729200074474127360_o.jpg?_nc_cat=102&_nc_ht=scontent-iad3-1.xx&oh=854dfcb941ef314013c8cf227b51151e&oe=5D422635"
     },
+    {
+        id: 4,
+        empty: false,
+        date: "Thursday, June 8th",
+        name: "Helen Lin",
+        time: "2:00 PM",
+        imgurl: "https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/28379635_1789696051093678_1971250718972312962_n.jpg?_nc_cat=105&_nc_ht=scontent-iad3-1.xx&oh=9d39fab20de090d194d815c55547717d&oe=5D17E785"
+    },
 ]
 
 export default class CalendarList extends React.Component {
 
-    _filterByActiveMonth() {
-        
-    }
-
     render() {
+      const filteredDATA = DATA.filter(obj => obj.date.includes(this.props.activeMonth))
+      if(filteredDATA.length === 0) {
+        return (
+          <Event key={-1}
+            empty={true}
+          />
+        );
+      }
       return (
           <ScrollView style={{flex: 1}}>
               {
-                  DATA.map((item, index) => (
+                  filteredDATA.map((item, index) => (
                       <Event key={item.id}
                         empty={item.empty}
                         date={item.date}
