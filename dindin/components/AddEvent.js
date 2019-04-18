322dff6a867bebeec23e5e2bdc70f282357fa2ce
William Li <wzl7zb@virginia.edu>
2019-04-18 12:45:02 -0400

    styling

--- dindin/components/AddEvent.js
+++ dindin/components/AddEvent.js
@@ -11,22 +11,37 @@ import {
 } from 'react-native';
 import { WebBrowser, Constants } from 'expo';
 import Picker from 'react-native-picker-view';
-
+[...Array(5).keys()]
 export default class AddEvent extends React.Component {
 
     render() {
       return (
-
-        <Picker
-        values={["Kolkata","Delhi","Hydrabad","Banglore","Pune"]}
-        //selected={this.state.index}
-        style={{width:100,height:200}}
-        enableInput={false}
-        onSelect={(value,index) => {
-            console.log('onSelect', value, index);
-            //this.setState()
-        }}
-    />
+        <View style={{
+                flex: 1,
+                flexDirection: 'row',
+                justifyContent: 'space-between'
+            }}>
+            <Picker
+            values={[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]}
+            //selected={this.state.index}
+            style={{width:100,height:200}}
+            enableInput={false}
+            onSelect={(value,index) => {
+                console.log('onSelect', value, index);
+                //this.setState()
+                }}
+            />
+            <Picker
+            values={[...Array(60).keys()]}
+            //selected={this.state.index}
+            style={{width:100,height:200}}
+            enableInput={false}
+            onSelect={(value,index) => {
+                console.log('onSelect', value, index);
+                //this.setState()
+                }}
+            />
+        </View>
       )
     }
 
